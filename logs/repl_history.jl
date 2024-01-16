c3ca003fd9bb6c510f246aebd8ff5623ac183b95
mylocyrus <YOUR_EMAILirahiggins740@gmail.com>
2024-01-16 04:19:26 -0800

    gitinit

---
+++ logs/repl_history.jl
@@ -0,0 +1,18 @@
+# time: 2024-01-15 08:47:12 Pacific Standard Time
+# mode: pkg
+	up
+# time: 2024-01-16 03:52:58 Pacific Standard Time
+# mode: julia
+	help
+# time: 2024-01-16 04:04:26 Pacific Standard Time
+# mode: julia
+	dir
+# time: 2024-01-16 04:04:32 Pacific Standard Time
+# mode: pkg
+	st
+# time: 2024-01-16 04:13:26 Pacific Standard Time
+# mode: pkg
+	]
+# time: 2024-01-16 04:13:31 Pacific Standard Time
+# mode: pkg
+	up
